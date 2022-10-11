a7f6412e01b75783c69739dbd02fb42762e4436b
jillkass <115515907+jillkass@users.noreply.github.com>
2022-10-10 23:32:35 -0700

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# config xray
+cat << EOF > /etc/config.json
+{
+  "inbounds":[
+    {
+      "port": $PORT,
+      "protocol": "vmess",
+      "settings": {
+        "decryption": "none",
+        "clients": [
+          {
+            "id": "e839d72c-5907-4071-a8e6-87496b1b3f98"
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws"
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
+}	
+EOF
+
+# run xray
+/usr/bin/xray run -config /etc/config.json
